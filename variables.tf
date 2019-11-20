67ae344c115bbf16ae7e6d3caf6e89cf98964691
andrefcpimentel2 <andre@hashicorp.com>
2019-11-20 13:41:22 +0000

    fixed annoying vars

--- variables.tf
+++ variables.tf
@@ -1,14 +1,10 @@
 variable "prefix" {
   description = "The prefix used for all resources in this example"
-  default     = "xl"
+  default     = "tf-azureapp-demo"
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
-}
-
-variable "subscription_id" {
-  description = "Azure Subscription ID to be used for billing"
 }
 
 variable "my_sql_master_password" {
